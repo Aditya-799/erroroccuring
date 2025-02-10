e4e65cf23a2a093f146ef23322357ca778ff9fe1
Aditya-799 <131836346+Aditya-799@users.noreply.github.com>
2025-02-10 20:33:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    res.status(200).json({info:'preset text'})
+app.get('/', (req, res) => {
+  res.status(200).json({info: 'preset text'})
 })
-app.post('/',(req,res)=>{
-    const {data}=req.body;
-    console.log(data);
+app.post('/', (req, res) => {
+  const {data} = req.body
+  console.log(data)
 })
-app.listen(4000,()=>{
-    console.log(__dirname);
-    console.log('server started running')
+app.listen(3000, () => {
+  console.log('server started running')
 })
